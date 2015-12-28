f5f0ea33907f6559d47da27f70f98ccc626d8ad0
Zoltán Borsos <zolli07@gmail.com>
2015-12-28 20:01:51 +0100

    *Added CollectionException and fixed non scalar inputs in Set.

---
+++ src/Exception/CollectionException.php
@@ -0,0 +1,28 @@
+<?php namespace BuildR\Collection\Exception;
+
+use BuildR\Foundation\Exception\Exception;
+
+/**
+ * This exception is thrown by any CollectionInterface implementation
+ *
+ * BuildR PHP Framework
+ *
+ * @author Zoltán Borsos <zolli07@gmail.com>
+ * @package Collection
+ * @subpackage Exception
+ *
+ * @copyright    Copyright 2015, Zoltán Borsos.
+ * @license      https://github.com/Zolli/BuildR/blob/master/LICENSE.md
+ * @link         https://github.com/Zolli/BuildR
+ *
+ * @codeCoverageIgnore
+ */
+class CollectionException extends Exception {
+
+    const MESSAGE_NON_SCALAR_TYPE = "This collection only store scalar types! %s given!";
+
+    public static function nonScalarTypeGiven($typeGiven) {
+        return self::createByFormat(self::MESSAGE_NON_SCALAR_TYPE, [$typeGiven]);
+    }
+
+}
--- src/Set.php
+++ src/Set.php
@@ -1,13 +1,32 @@
 <?php namespace BuildR\Collection;
 
+use BuildR\Collection\Exception\CollectionException;
 use BuildR\Collection\Interfaces\CollectionInterface;
 
+/**
+ * Set type (Collection implementation)
+ * Sets only store scalar types
+ *
+ * BuildR PHP Framework
+ *
+ * @author Zoltán Borsos <zolli07@gmail.com>
+ * @package Collection
+ *
+ * @copyright    Copyright 2015, Zoltán Borsos.
+ * @license      https://github.com/Zolli/BuildR/blob/master/LICENSE.md
+ * @link         https://github.com/Zolli/BuildR
+ */
 class Set implements CollectionInterface {
 
-    private $data = [];
+    protected $data = [];
 
     private $position = 0;
 
+    /**
+     * Set constructor.
+     *
+     * @param null|array $values
+     */
     public function __construct($values = NULL) {
         if(is_array($values)) {
             $this->addAll($values);
@@ -25,6 +44,10 @@ class Set implements CollectionInterface {
      * @inheritDoc
      */
     public function add($element) {
+        if(!is_scalar($element)) {
+            throw CollectionException::nonScalarTypeGiven(gettype($element));
+        }
+
         $this->data[] = $element;
     }
 
@@ -50,11 +73,7 @@ class Set implements CollectionInterface {
      * @inheritDoc
      */
     public function contains($element) {
-        if(isset($this->data[$element])) {
-            return TRUE;
-        }
-
-        return FALSE;
+        return (array_search($element, $this->data, TRUE) === FALSE) ? FALSE : TRUE;
     }
 
     /**
